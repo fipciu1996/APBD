3fd18fb09d5db00303ecd8a4b80b2ef3ca0eb6ed
Mateusz Filipek <fipciu1996@gmail.com>
2020-03-29 17:55:59 +0200

    Work on classes

---
+++ WebApplication1/Controllers/StudentsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{   
+   
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public String GetStudents([FromQuery] String orderBy)
+        {
+            return $"Kowalski, Malewski, Andrzejewski sortowanie = {orderBy}";
+        }
+
+        [HttpPost]
+        public IActionResult AddStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 2000)}";
+            return Ok(student);
+        }
+
+        [HttpGet("student/{id:int}")]
+        public IActionResult GetStudentById([FromRoute] int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            } else
+            {
+                return NotFound("Nie ma takiego studenta");
+            }
+            
+        }
+    }
+}

d1209df397221b79887e1bc276806dcc25e5628c
Mateusz Filipek <fipciu1996@gmail.com>
2020-04-02 22:04:42 +0200

    Zadanie 7 zgodnie z trescia

--- WebApplication1/Controllers/StudentsController.cs
+++ WebApplication1/Controllers/StudentsController.cs
@@ -6,22 +6,27 @@ using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
-{   
-   
+{
+
     [ApiController]
     [Route("api/students")]
     public class StudentsController : ControllerBase
     {
+            using (var con = new SqlConnection("Data Source=localhost,1433;Initial Catalog=master;User ID=sa;Password=Mssql1234!"))
+            using (var com = new SqlCommand())
+
+
         [HttpGet]
         public String GetStudents([FromQuery] String orderBy)
         {
+
             return $"Kowalski, Malewski, Andrzejewski sortowanie = {orderBy}";
         }
 
         [HttpPost]
         public IActionResult AddStudent(Student student)
         {
-            student.IndexNumber = $"s{new Random().Next(1, 2000)}";
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
             return Ok(student);
         }
 

cb1b736547fe6b6c019ec40fef010b2434909eb6
Mateusz Filipek <fipciu1996@gmail.com>
2020-04-03 20:26:37 +0200

    Zadania 4.2 i 4.3

--- WebApplication1/Controllers/StudentsController.cs
+++ WebApplication1/Controllers/StudentsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,12 +15,10 @@ namespace WebApplication1.Controllers
     public class StudentsController : ControllerBase
     {
 
-
         [HttpGet]
-        public String GetStudents([FromQuery] String orderBy)
+        public List<Student> GetStudents()
         {
-
-            return $"Kowalski, Malewski, Andrzejewski sortowanie = {orderBy}";
+            return SelectAllStudents();
         }
 
         [HttpPut("student/{id:int}")]
@@ -38,22 +37,68 @@ namespace WebApplication1.Controllers
         [HttpPost]
         public IActionResult AddStudent(Student student)
         {
-            
             student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            student.AddStudent();
             return Ok(student);
         }
 
         [HttpGet("student/{id:int}")]
         public IActionResult GetStudentById([FromRoute] int id)
         {
-            if (id == 1)
+            Student student = SelectStudentById(id);
+            if (!student.IdStudent.Equals(""))
             {
-                return Ok("Kowalski");
+                return Ok(student);
             } else
             {
                 return NotFound("Nie ma takiego studenta");
             }
             
+        }
+
+        private Student SelectStudentById(int id)
+        {
+            using (var con = new SqlConnection("Data Source=localhost,1433;Initial Catalog=master;User ID=sa;Password=Mssql1234!"))
+            using (var com = new SqlCommand())
+            {
+                com.Connection = con;
+                com.CommandText = "SELECT * FROM Students WHERE IdStudent = @IdStudent";
+                com.Parameters.Add("@IdStudent", SqlDbType.Int).Value = id;
+                con.Open();
+                var dr = com.ExecuteReader();
+                var st = new Student();
+                while (dr.Read())
+                {
+                    st.IdStudent = int.Parse(dr["IdStudent"].ToString());
+                    st.FirstName = dr["FirstName"].ToString();
+                    st.LastName = dr["LastName"].ToString();
+                    st.IndexNumber = dr["IndexNumber"].ToString();
+                }
+                return st;
+            }
+        }
+
+        public List<Student> SelectAllStudents()
+        {
+            using (var con = new SqlConnection("Data Source=localhost,1433;Initial Catalog=master;User ID=sa;Password=Mssql1234!"))
+            using (var com = new SqlCommand())
+            {
+                com.Connection = con;
+                com.CommandText = "SELECT * FROM Students";
+                con.Open();
+                var dr = com.ExecuteReader();
+                List<Student> students = new List<Student>();
+                while (dr.Read())
+                {
+                    var st = new Student();
+                    st.IdStudent = int.Parse(dr["IdStudent"].ToString());
+                    st.FirstName = dr["FirstName"].ToString();
+                    st.LastName = dr["LastName"].ToString();
+                    st.IndexNumber = dr["IndexNumber"].ToString();
+                    students.Add(st);
+                }
+                return students;
+            }
         }
     }
 }
--- WebApplication1/Models/Student.cs
+++ WebApplication1/Models/Student.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +9,37 @@ namespace WebApplication1.Models
 {
     public class Student
     {
+
+
         public int IdStudent { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string IndexNumber { get; set; }
+
+        public void AddStudent()
+        {
+            string connetionString = null;
+            string sql = null;
+
+            // All the info required to reach your db. See connectionstrings.com
+            connetionString = "Data Source = localhost,1433; Initial Catalog = master; User ID = sa; Password = Mssql1234!";
+
+            // Prepare a proper parameterized query 
+            sql = "insert into Students (IdStudent, FirstName, LastName, IndexNumber ) values (@IdStudent, @FirstName, @LastName, @IndexNumber)";
+
+            // Create the connection (and be sure to dispose it at the end)
+            using (SqlConnection cnn = new SqlConnection(connetionString))
+            {
+                cnn.Open();
+                using (SqlCommand cmd = new SqlCommand(sql, cnn))
+                {
+                    cmd.Parameters.Add("@IdStudent", SqlDbType.Int).Value = IdStudent;
+                    cmd.Parameters.Add("@FirstName", SqlDbType.NVarChar).Value = FirstName;
+                    cmd.Parameters.Add("@LastName", SqlDbType.NVarChar).Value = LastName;
+                    cmd.Parameters.Add("@IndexNumber", SqlDbType.NVarChar).Value = IndexNumber;
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
